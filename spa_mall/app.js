d897d08edf9dc310df58cd3fb86042407cc53c4f
Justin-SJ-Hong <madwolves98@gmail.com>
2022-08-24 21:15:16 +0900

    Merge branch 'main' of https://github.com/Justin-SJ-Hong/Sparta_NodeJS into main

--- spa_mall/app.js
+++ spa_mall/app.js
@@ -1,17 +1,22 @@
 const express = require("express");
+const connect = require("./schemas")
 const app = express();
 const port = 3000;
 
-const goodsRouter = require("./routes/goods.js");
+connect();
+
+const goodsRouter = require("./routes/goods");
+const cartsRouter = require("./routes/cart");
 
 const requestMiddleware = (req, res, next) => {
     console.log("Request URL:", req.originalUrl, " - ", new Date());
     next();
 };
 
+app.use(express.json())
 app.use(requestMiddleware);
 
-app.use("/api", [goodsRouter]);
+app.use("/api", [goodsRouter, cartsRouter]);
 
 app.get("/", (req, res) => {
     res.send("Hello World@@@@");
